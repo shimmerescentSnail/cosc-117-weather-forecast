b141356e2f8d1c77fec333b0889df6d68a4bb520
shimmerescentSnail <michael.bell1@myokanagan.bc.ca>
2025-11-16 19:12:25 -0800

    Adds test_backend.py v1

---
+++ test_backend.py
@@ -0,0 +1,34 @@
+from backend import calc_aqi_level
+from config import AQI_INTERVALS
+
+def test_so2_level_calculation():
+
+    """
+
+    Tests the case we found in the debugger.
+
+    SO2 concentration of "77" should be Level 2 ("Good").
+
+    """
+
+    concentration = "77"
+
+    intervals = AQI_INTERVALS["so2"]
+
+
+    # We expect Level 2 (Good)
+
+    expected_level = 2
+
+    calculated_level = calc_aqi_level(concentration, intervals)
+
+    assert calculated_level == expected_level
+
+
+def test_co_level_calculation():
+    concentration = "684.85"
+    intervals=AQI_INTERVALS["co"]
+
+    expected_level = 1
+    calculated_level = calc_aqi_level(concentration, intervals)
+    assert calculated_level == expected_level
